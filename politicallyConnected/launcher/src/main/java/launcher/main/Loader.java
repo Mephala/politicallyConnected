9e8c9dbd2a16797db5b48feae361ae6b41f2fc24
Mephala <gokhan.ozgozen@gmail.com>
2015-03-27 23:22:54 +0200

    BugFixing stage -1 . Must finish this and clean up!!!

--- politicallyConnected/launcher/src/main/java/launcher/main/Loader.java
+++ politicallyConnected/launcher/src/main/java/launcher/main/Loader.java
@@ -2,8 +2,8 @@ package launcher.main;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.UUID;
@@ -22,71 +22,93 @@ public class Loader {
 	private static final String MAIN_APP_NAME_TO_DOWNLOAD = "mainApp.jar";
 
 	public static void main(String[] args) throws InterruptedException {
-		final LauncherView launcherView;
-		try {
-			ExecutorService executor = Executors.newCachedThreadPool();
-			final LoadingData loadingData = LoadingData.constructLoadingData();
-			loadingData.setInfo("Checking internet connection ...");
-			Future<LaunchViewStartupResult> launchResultFuture = executor.submit(new Callable<LaunchViewStartupResult>() {
-				public LaunchViewStartupResult call() throws Exception {
-					LauncherViewLoader viewLoader = new LauncherViewLoader();
-					return viewLoader.launchView(loadingData);
-				}
-			});
-			Long totalData = ServiceClient.getFileSizeForDownload(MAIN_APP_NAME_TO_DOWNLOAD);
-			boolean connectionSuccess = !totalData.equals(Long.valueOf(-1));
-			if (connectionSuccess) {
-				while (!launchResultFuture.isDone())
-					;
-				LaunchViewStartupResult startupResult = launchResultFuture.get();
-				if (startupResult.isSuccess) {
-					launcherView = startupResult.lw;
-					loadingData.setTotalData(totalData);
-					loadingData.getDownloadStarted().set(true);
-					loadingData.setInfo("Connected. Downloading updates...");
-					String fileName = UUID.randomUUID().toString() + ".jar";
-					URL link = new URL("http://kelepirpc.com/serviceProvider/" + MAIN_APP_NAME_TO_DOWNLOAD + "/downloadFile.do");
-					InputStream in = new BufferedInputStream(link.openStream());
-					ByteArrayOutputStream out = new ByteArrayOutputStream();
-					byte[] buf = new byte[1024];
-					int n = 0;
-					long processData = 0l;
-					while (-1 != (n = in.read(buf))) {
-						loadingData.setInfo("Downloading app data...");
-						loadingData.setProcessedData(processData);
-						out.write(buf, 0, n);
-						processData += 1024;
+		if (args == null || args.length == 0) {
+			final LauncherView launcherView;
+			try {
+				ExecutorService executor = Executors.newCachedThreadPool();
+				final LoadingData loadingData = LoadingData.constructLoadingData();
+				loadingData.setInfo("Checking internet connection ...");
+				Future<LaunchViewStartupResult> launchResultFuture = executor.submit(new Callable<LaunchViewStartupResult>() {
+					public LaunchViewStartupResult call() throws Exception {
+						LauncherViewLoader viewLoader = new LauncherViewLoader();
+						return viewLoader.launchView(loadingData);
 					}
-					loadingData.setInfo("App is downloaded successfully.");
-					loadingData.setProcessedData(totalData);
-					out.close();
-					in.close();
-					byte[] response = out.toByteArray();
-					FileOutputStream fos = new FileOutputStream(fileName);
-					fos.write(response);
-					fos.close();
-					Process proc = Runtime.getRuntime().exec("java -jar " + fileName);
-					launcherView.setVisible(false);
-					int retVal = proc.waitFor();
-					if (retVal != 0)
-						JOptionPane.showMessageDialog(null, "Meydana gelen bir hatadan dolayı app kapandı. Gökhan Özgözen ile irtibata geçiniz!", "HATA!!!!", JOptionPane.ERROR_MESSAGE);
-					File file = new File(fileName);
-					boolean fileDeleteSuccess = file.delete();
-					if (!fileDeleteSuccess)
-						JOptionPane.showMessageDialog(null, fileName + " isimli uygulama silinmelidir, otomatik olarak silinirken bir hata olustu, lutfen siz siliniz....", "HATA!!!!", JOptionPane.ERROR_MESSAGE);
+				});
+				Long totalData = ServiceClient.getFileSizeForDownload(MAIN_APP_NAME_TO_DOWNLOAD);
+				boolean connectionSuccess = !totalData.equals(Long.valueOf(-1));
+				if (connectionSuccess) {
+					while (!launchResultFuture.isDone())
+						;
+					LaunchViewStartupResult startupResult = launchResultFuture.get();
+					if (startupResult.isSuccess) {
+						launcherView = startupResult.lw;
+						loadingData.setTotalData(totalData);
+						loadingData.getDownloadStarted().set(true);
+						loadingData.setInfo("Connected. Downloading updates...");
+						final String fileName = UUID.randomUUID().toString() + ".jar";
+						URL link = new URL("http://localhost:8080/" + MAIN_APP_NAME_TO_DOWNLOAD + "/downloadFile.do");
+						InputStream in = new BufferedInputStream(link.openStream());
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						byte[] buf = new byte[1024];
+						int n = 0;
+						long processData = 0l;
+						while (-1 != (n = in.read(buf))) {
+							loadingData.setInfo("Downloading app data...");
+							loadingData.setProcessedData(processData);
+							out.write(buf, 0, n);
+							processData += 1024;
+						}
+						loadingData.setInfo("App is downloaded successfully.");
+						loadingData.setProcessedData(totalData);
+						out.close();
+						in.close();
+						byte[] response = out.toByteArray();
+						FileOutputStream fos = new FileOutputStream(fileName);
+						fos.write(response);
+						fos.close();
+						Executors.newCachedThreadPool().submit(new Runnable() {
+
+							public void run() {
+								try {
+									Process proc = Runtime.getRuntime().exec("java -jar " + fileName + " " + fileName);
+								} catch (IOException e) {
+									JOptionPane.showMessageDialog(null, "Reeskont");
+								}
+
+							}
+						});
+
+						launcherView.setVisible(false);
+						// int retVal = proc.waitFor();
+						// if (retVal != 0)
+						// JOptionPane.showMessageDialog(null,
+						// "Meydana gelen bir hatadan dolayı app kapandı. Gökhan Özgözen ile irtibata geçiniz!",
+						// "HATA!!!!",
+						// JOptionPane.ERROR_MESSAGE);
+						// File file = new File(fileName);
+						// boolean fileDeleteSuccess = file.delete();
+						// if (!fileDeleteSuccess)
+						// JOptionPane.showMessageDialog(null, fileName +
+						// " isimli uygulama silinmelidir, otomatik olarak silinirken bir hata olustu, lutfen siz siliniz....",
+						// "HATA!!!!", JOptionPane.ERROR_MESSAGE);
+					} else {
+						JOptionPane.showMessageDialog(null, "Error occured during loader window startup. Error message:" + startupResult.error
+								+ ". Contact to Gökhan Özgözen: gokhan.ozgozen@gmail.com", "HATA!!!!", JOptionPane.ERROR_MESSAGE);
+						System.exit(-1);
+					}
 				} else {
-					JOptionPane.showMessageDialog(null, "Error occured during loader window startup. Error message:" + startupResult.error + ". Contact to Gökhan Özgözen: gokhan.ozgozen@gmail.com", "HATA!!!!", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(null, "Connection failure. Either you are offline or the servers are down. Contact to Gökhan Özgözen: gokhan.ozgozen@gmail.com",
+							"HATA!!!!", JOptionPane.ERROR_MESSAGE);
 					System.exit(-1);
 				}
-			} else {
-				JOptionPane.showMessageDialog(null, "Connection failure. Either you are offline or the servers are down. Contact to Gökhan Özgözen: gokhan.ozgozen@gmail.com", "HATA!!!!", JOptionPane.ERROR_MESSAGE);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "HATA!!!!", JOptionPane.ERROR_MESSAGE);
 				System.exit(-1);
+			} finally {
+				System.exit(0);
 			}
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getMessage(), "HATA!!!!", JOptionPane.ERROR_MESSAGE);
-			System.exit(-1);
-		} finally {
-			System.exit(0);
+		} else {
+			JOptionPane.showMessageDialog(null, args[0]);
 		}
 	}
 }
--- politicallyConnected/mainApp/src/main/java/mainApp/view/PcaFrame.java
+++ politicallyConnected/mainApp/src/main/java/mainApp/view/PcaFrame.java
@@ -1,11 +1,14 @@
 package mainApp.view;
 
 import java.awt.EventQueue;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
+import java.util.concurrent.Executors;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -28,24 +31,48 @@ public class PcaFrame extends JFrame {
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					System.setProperty("file.encoding", "UTF-8");
-					Field charset = Charset.class.getDeclaredField("defaultCharset");
-					charset.setAccessible(true);
-					charset.set(null, null);
-					PcaFrame frame = new PcaFrame();
-					frame.setTitle("Politically Connected Software --- Gokhan Ozgozen");
-					ImageIcon img = MainAppUtils.createImageIcon("/Hero_Adelaide.png", "Adelaide");
-					if (img != null)
-						frame.setIconImage(img.getImage());
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+		if (args == null || args.length == 0) {
+			JOptionPane.showMessageDialog(null, "Program doğru parametrelerle başlatılmadı.", "Olmadi Bu is", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		} else {
+			final String jarName = args[0];
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						System.setProperty("file.encoding", "UTF-8");
+						Field charset = Charset.class.getDeclaredField("defaultCharset");
+						charset.setAccessible(true);
+						charset.set(null, null);
+						final PcaFrame frame = new PcaFrame();
+						frame.setTitle("Politically Connected Software --- Gokhan Ozgozen");
+						ImageIcon img = MainAppUtils.createImageIcon("/Hero_Adelaide.png", "Adelaide");
+						if (img != null)
+							frame.setIconImage(img.getImage());
+						frame.addWindowListener(new java.awt.event.WindowAdapter() {
+							@Override
+							public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+								if (JOptionPane.showConfirmDialog(frame, "Are you sure to close this window?", "Really Closing?", JOptionPane.YES_NO_OPTION,
+										JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+									Executors.newCachedThreadPool().submit(new Runnable() {
+										public void run() {
+											try {
+												Process proc = Runtime.getRuntime().exec("java -jar pcaLoader.jar " + jarName);
+											} catch (IOException e) {
+												JOptionPane.showMessageDialog(null, "Reeskont");
+											}
+										}
+									});
+									System.exit(0);
+								}
+							}
+						});
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
-			}
-		});
+			});
+		}
 	}
 
 	/**
